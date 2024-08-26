a1158dd524a98b35b880ada97b3000045d7d368a
Dev-Bisi <oyebisioluwagbemiga@gmail.com>
2024-08-26 17:40:22 +0100

    My Bakery Website Created with HTML CSS BOOTSTRAP JAVASCRIPT

---
+++ index.js
@@ -0,0 +1,9 @@
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var carouselElement = document.getElementById('carouselExample');
+        var carousel = new bootstrap.Carousel(carouselElement, {
+            interval: 2000,
+            wrap: true
+        });
+    });
+
